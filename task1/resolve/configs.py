c7f2cd595388fcff6fa40609005386fd41244a3e
Chazarow <zaharowanrew@gmail.com>
2025-09-29 11:46:26 +0400

    chore: clear docs

--- task1/resolve/configs.py
+++ task1/resolve/configs.py
@@ -5,23 +5,18 @@ from typing import Tuple
 class Settings(BaseSettings):
     """Настройки приложения"""
     
-    # Настройки GUI
     window_title: str = "Обработка изображений"
     window_theme: str = "LightBlue3"
     max_image_display_size: Tuple[int, int] = (800, 600)
     
-    # Настройки обработки изображений
     default_brightness: float = 0.0
     default_contrast: float = 1.0
     default_saturation: float = 1.0
     default_gamma: float = 1.0
     
-    # Поддерживаемые форматы изображений
     supported_image_extensions: Tuple[str, ...] = (
         ".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".tif", ".gif", ".webp"
     )
-    
-    # Настройки гистограммы
     histogram_bins: int = 256
     histogram_figure_size: Tuple[int, int] = (10, 6)
     histogram_dpi: int = 100
